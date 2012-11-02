b21b4c9683e645b0ad9ac2cd623bb77ff8380464
hataji <hatakyo12@gmail.com>
2012-11-02 22:35:49 +0900

    pass chenge

--- app/webroot/themed/it1616_com/elements/toppage.php
+++ app/webroot/themed/it1616_com/elements/toppage.php
@@ -4,7 +4,7 @@
 <div>
     <?php $bcBaser->content() ?><!--TOPページ-->
 </div>
-<div id="top-okazaki"><?php $bcBaser->getimg('./top/top_taro.png',array('url' => 'okazakitarou')); ?></div>
+<div id="top-okazaki"><?php $bcBaser->img('./top/top_taro.png',array('url' => '/okazakitarou')); ?></div>
 <div id="top-contents">
 	<div id="top-left">
 		<img alt="" src="/themed/it1616_com/img/top/top_itm.png" /><img alt="" src="/themed/it1616_com/img/top/top_ms.png" /></div>
@@ -14,24 +14,24 @@
 
 <div id="top-magazine">
     <div id="top-magazine-text">通販で、インターネットで。<br />ビジネスに役立つ独自のアイディアをご紹介。<br />過去に配信した８００通を超えるメールマガジンをWEB限定公開！</div>
-    <div id="top-magazine-btn"><?php $bcBaser->img('./top/mail_b.png',array('url' => 'magazine')); ?></div>
+    <div id="top-magazine-btn"><?php $bcBaser->img('./top/mail_b.png',array('url' => '/magazine')); ?></div>
 </div>
 
 <div id="media-news">
     <div id="media-news-title">メディア・ニュース　<span>岡崎太郎が雑誌や新聞で紹介されました</span></div>
     <div id="media-news-con">
         <ul>
-            <li><?php $bcBaser->img('./top/media/media_01.png',array('url' => 'media_01')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_02.png',array('url' => 'media_02')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_03.png',array('url' => 'media_03')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_04.png',array('url' => 'media_04')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_05.png',array('url' => 'media_05')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_06.png',array('url' => 'media_06')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_07.png',array('url' => 'media_07')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_08.png',array('url' => 'media_08')); ?></li>
-            <li><?php $bcBaser->img('./top/media/media_09.png',array('url' => 'media_09')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_01.png',array('url' => '/media_01')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_02.png',array('url' => '/media_02')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_03.png',array('url' => '/media_03')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_04.png',array('url' => '/media_04')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_05.png',array('url' => '/media_05')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_06.png',array('url' => '/media_06')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_07.png',array('url' => '/media_07')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_08.png',array('url' => '/media_08')); ?></li>
+            <li><?php $bcBaser->img('./top/media/media_09.png',array('url' => '/media_09')); ?></li>
         </ul>
     </div>
 </div>
     
-<div id="top-contact"><?php $bcBaser->img('./top/tel_l.png',array('url' => 'contact')); ?></div>
+<div id="top-contact"><?php $bcBaser->img('./top/tel_l.png',array('url' => '/contact')); ?></div>
